cc141b2490ae91d7e13aba33249b5b7fc92d4111
Barry Jacobs <barry.jacobs@mechenet.com>
2024-03-23 17:24:15 +0000

    Removed comment

--- lib/main.ts
+++ lib/main.ts
@@ -3,5 +3,3 @@ import { Calendar } from "./components/Calendar/Calendar"
 import { InputWidth, InputWidthClass } from "./types"
 
 export { DatePicker, Calendar, InputWidth, InputWidthClass }
-
-// import "./styles/styles.scss"
